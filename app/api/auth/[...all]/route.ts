dd4f43fda8f271d74a8d145731645fb289816c95
abdalegurey <abdalegureyguled@gmail.com>
2026-01-06 13:38:10 +0300

    fix(auth): fully disable build-time evaluation for BetterAuth route

--- app/api/auth/[...all]/route.ts
+++ app/api/auth/[...all]/route.ts
@@ -1,7 +1,10 @@
+// app/api/auth/[...all]/route.ts
+
 export const dynamic = "force-dynamic";
 export const runtime = "nodejs";
-
+export const revalidate = 0;
 
 import { auth } from "@/lib/auth";
 import { toNextJsHandler } from "better-auth/next-js";
+
 export const { GET, POST } = toNextJsHandler(auth);
